eab119142221b2bbb674f62542aa97f1ea3fc291
Pierre Karashchuk <krchtchk@gmail.com>
2020-08-16 12:29:46 -0700

    move generated files to docs

--- scripts/generate.py
+++ scripts/generate.py
@@ -17,14 +17,14 @@ def convert_org_file(fname):
            "--eval", '(batch-org-to-html "{}")'.format(fname)]
     subprocess.run(cmd, check=True)
 
-def adapt_link(link):
-    if 'href' not in link.attrs: return
-    href = link.attrs['href']
+def adapt_link(href):
     dirname = os.path.dirname(href)
     # don't change external links
-    if dirname[:4] == 'http': return
+    if dirname[:4] == 'http':
+        return href
     # don't change id links
-    if href[0] == '#': return
+    if href[0] == '#':
+        return href
     # turn relative into absolute paths
     if href[0] != '/':
         href = '/' + href
@@ -32,16 +32,19 @@ def adapt_link(link):
     base, ext = os.path.splitext(href)
     if href != '/index.html' and ext == '.html': #
         href = base
-    # set the link
-    link.attrs['href'] = href
-
+    return href
 
 def process_html(fname):
     with open(fname, 'r') as f:
         soup = BeautifulSoup(f, 'lxml')
     links = soup.find_all('a')
     for link in links:
-        adapt_link(link)
+        if 'href' in link.attrs:
+            link.attrs['href'] = adapt_link(link.attrs['href'])
+    images = soup.find_all('img')
+    for img in images:
+        if 'src' in img.attrs:
+            img.attrs['src'] = adapt_link(img.attrs['src'])
     with open(fname, 'w') as f:
         f.write(str(soup))
 
@@ -51,9 +54,9 @@ fnames = glob("*.org")
 for fname in fnames:
     basename, ext = os.path.splitext(fname)
     if basename != 'index':
-        outname = os.path.join(basename, 'index.html')
+        outname = os.path.join("docs", basename, 'index.html')
     else:
-        outname = "index.html"
+        outname = os.path.join("docs", "index.html")
 
     if os.path.exists(outname) and \
        os.path.getmtime(outname) > os.path.getmtime(fname):
@@ -62,7 +65,6 @@ for fname in fnames:
     print('converting {}'.format(fname))
     convert_org_file(fname)
     # move to folder
-    if basename != 'index':
-        os.makedirs(basename, exist_ok=True)
-        shutil.move(basename+'.html', outname)
+    os.makedirs(os.path.dirname(outname), exist_ok=True)
+    shutil.move(basename+'.html', outname)
     process_html(outname)
